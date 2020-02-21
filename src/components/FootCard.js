b5353a81c1dbdb615dc5364233bef9fe7c9c54a0
Torrence Cole <torrencecole@Torrences-MacBook-Pro.local>
2020-02-21 09:48:30 -0500

    Photos and Adjustment to portfolio component

--- src/components/FootCard.js
+++ src/components/FootCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
@@ -8,9 +9,9 @@ function FootCard() {
         <Col>
             <Card body="true" className="card-body border-0 o-hidden mb-0 bg-primary text-light">
                 <div className="position-relative d-flex flex-column flex-md-row justify-content-between align-items-center">
-                <div className="h3 text-center mb-md-0">Some cool callout to think about</div>
+                <div className="h3 text-center mb-md-0">Have a project you would like to discuss?</div>
                     <Button size="lg" variant="light">
-                        Clicker if needed
+                        <Link to="/contact">Hire Torrence!</Link>
                     </Button>
                 </div>
             </Card>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,6 +6,11 @@ function Portfolio() {
         <Row noGutters="true" className="App-header page">
             <h1 className="fade-in">Portfolio</h1>
             <img src="/img/sandglo-fold.png" />
+            <img src="/img/mileti-fold.png" />
+            <img src="/img/ahern-fold.png" />
+            <img src="/img/impressions-fold.png" />
+            <img src="/img/jan-fold.png" />
+            <img src="/img/skyco-fold.png" />
         </Row>
     )
 }
